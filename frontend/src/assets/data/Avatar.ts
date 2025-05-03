5a6fed662f9b324f0d8d51665a819ca2cbdcd848
Tayssirar <tayssirabdelkader1@gmail.com>
2025-05-03 21:09:49 +0100

    added a list of avatar

--- frontend/src/assets/data/Avatar.ts
+++ frontend/src/assets/data/Avatar.ts
@@ -8,7 +8,32 @@ import avatar2_profile from "../avatars/avatar2/profile.png";
 import avatar2_welcome from "../avatars/avatar2/welcome.png";
 import avatar2_cheerful from "../avatars/avatar2/cheerful.png";
 import avatar2_sad from "../avatars/avatar2/sad.png";
-import avatar3_profile from "../avatars/avatar3/profile.png";
+import avatar3_profile from "../avatars/avatar3.png";
+import avatar4_profile from "../avatars/avatar4.png";
+import avatar5_profile from "../avatars/avatar5.png";
+import avatar6_profile from "../avatars/avatar6.png";
+import avatar7_profile from "../avatars/avatar7.png";
+import avatar8_profile from "../avatars/avatar8.png";
+import avatar9_profile from "../avatars/avatar9.png";
+import avatar10_profile from "../avatars/avatar10.png";
+import avatar11_profile from "../avatars/avatar11.png";
+import avatar12_profile from "../avatars/avatar12.png";
+import avatar13_profile from "../avatars/avatar13.png";
+import avatar14_profile from "../avatars/avatar14.png";
+import avatar15_profile from "../avatars/avatar15.png";
+import avatar16_profile from "../avatars/avatar16.png";
+import avatar17_profile from "../avatars/avatar17.png";
+import avatar18_profile from "../avatars/avatar18.png";
+import avatar19_profile from "../avatars/avatar19.png";
+import avatar20_profile from "../avatars/avatar20.png";
+import avatar21_profile from "../avatars/avatar21.png";
+import avatar22_profile from "../avatars/avatar22.png";
+import avatar23_profile from "../avatars/avatar23.png";
+import avatar24_profile from "../avatars/avatar24.png";
+import avatar25_profile from "../avatars/avatar25.png";
+import avatar26_profile from "../avatars/avatar26.png";
+import avatar27_profile from "../avatars/avatar27.png";
+
 
 // Repeat for other avatars...
 
@@ -38,4 +63,205 @@ export const avatars: Avatar[] = [
     cheerful: avatar2_cheerful,
     sad: avatar2_sad,
   },
+  {
+    id: "avatar3",
+    name: "Avatar 3",
+    profile: avatar3_profile,
+    welcome: avatar3_profile,
+    cheerful: avatar3_profile,
+    sad: avatar3_profile,
+  },
+  {
+    id: "avatar4",
+    name: "Avatar 4",
+    profile: avatar4_profile,
+    welcome: avatar4_profile,
+    cheerful: avatar4_profile,
+    sad: avatar4_profile,
+  },
+  {
+    id: "avatar5",
+    name: "Avatar 5",
+    profile: avatar5_profile,
+    welcome: avatar5_profile,
+    cheerful: avatar5_profile,
+    sad: avatar5_profile,
+  },
+  {
+    id: "avatar6",
+    name: "Avatar 6",
+    profile: avatar6_profile,
+    welcome: avatar6_profile,
+    cheerful: avatar6_profile,
+    sad: avatar6_profile,
+  },
+  {
+    id: "avatar7",
+    name: "Avatar 7",
+    profile: avatar7_profile,
+    welcome: avatar7_profile,
+    cheerful: avatar7_profile,
+    sad: avatar7_profile,
+  },
+  {
+    id: "avatar8",
+    name: "Avatar 8",
+    profile: avatar8_profile,
+    welcome: avatar8_profile,
+    cheerful: avatar8_profile,
+    sad: avatar8_profile,
+  },
+  {
+    id: "avatar9",
+    name: "Avatar 9",
+    profile: avatar9_profile,
+    welcome: avatar9_profile,
+    cheerful: avatar9_profile,
+    sad: avatar9_profile,
+  },
+  {
+    id: "avatar10",
+    name: "Avatar 10",
+    profile: avatar10_profile,
+    welcome: avatar10_profile,
+    cheerful: avatar10_profile,
+    sad: avatar10_profile,
+  },
+  {
+    id: "avatar11",
+    name: "Avatar 11",
+    profile: avatar11_profile,
+    welcome: avatar11_profile,
+    cheerful: avatar11_profile,
+    sad: avatar11_profile,
+  },
+  {
+    id: "avatar12",
+    name: "Avatar 12",
+    profile: avatar12_profile,
+    welcome: avatar12_profile,
+    cheerful: avatar12_profile,
+    sad: avatar12_profile,
+  },
+  {
+    id: "avatar13",
+    name: "Avatar 13",
+    profile: avatar13_profile,
+    welcome: avatar13_profile,
+    cheerful: avatar13_profile,
+    sad: avatar13_profile,
+  },
+  {
+    id: "avatar14",
+    name: "Avatar 14",
+    profile: avatar14_profile,
+    welcome: avatar14_profile,
+    cheerful: avatar14_profile,
+    sad: avatar14_profile,
+  },
+  {
+    id: "avatar15",
+    name: "Avatar 15",
+    profile: avatar15_profile,
+    welcome: avatar15_profile,
+    cheerful: avatar15_profile,
+    sad: avatar15_profile,
+  },
+  {
+    id: "avatar16",
+    name: "Avatar 16",
+    profile: avatar16_profile,
+    welcome: avatar16_profile,
+    cheerful: avatar16_profile,
+    sad: avatar16_profile,
+  },
+  {
+    id: "avatar17",
+    name: "Avatar 17",
+    profile: avatar17_profile,
+    welcome: avatar17_profile,
+    cheerful: avatar17_profile,
+    sad: avatar17_profile,
+  },
+  {
+    id: "avatar18",
+    name: "Avatar 18",
+    profile: avatar18_profile,
+    welcome: avatar18_profile,
+    cheerful: avatar18_profile,
+    sad: avatar18_profile,
+  },
+  {
+    id: "avatar19",
+    name: "Avatar 19",
+    profile: avatar19_profile,
+    welcome: avatar19_profile,
+    cheerful: avatar19_profile,
+    sad: avatar19_profile,
+  },
+  {
+    id: "avatar20",
+    name: "Avatar 20",
+    profile: avatar20_profile,
+    welcome: avatar20_profile,
+    cheerful: avatar20_profile,
+    sad: avatar20_profile,
+  },
+  {
+    id: "avatar21",
+    name: "Avatar 21",
+    profile: avatar21_profile,
+    welcome: avatar21_profile,
+    cheerful: avatar21_profile,
+    sad: avatar21_profile,
+  },
+  {
+    id: "avatar22",
+    name: "Avatar 22",
+    profile: avatar22_profile,
+    welcome: avatar22_profile,
+    cheerful: avatar22_profile,
+    sad: avatar22_profile,
+  },
+  {
+    id: "avatar23",
+    name: "Avatar 23",
+    profile: avatar23_profile,
+    welcome: avatar23_profile,
+    cheerful: avatar23_profile,
+    sad: avatar23_profile,
+  },
+  {
+    id: "avatar24",
+    name: "Avatar 24",
+    profile: avatar24_profile,
+    welcome: avatar24_profile,
+    cheerful: avatar24_profile,
+    sad: avatar24_profile,
+  },
+  {
+    id: "avatar25",
+    name: "Avatar 25",
+    profile: avatar25_profile,
+    welcome: avatar25_profile,
+    cheerful: avatar25_profile,
+    sad: avatar25_profile,
+  },
+  {
+    id: "avatar26",
+    name: "Avatar 26",
+    profile: avatar26_profile,
+    welcome: avatar26_profile,
+    cheerful: avatar26_profile,
+    sad: avatar26_profile,
+  },
+  {
+    id: "avatar27",
+    name: "Avatar 27",
+    profile: avatar27_profile,
+    welcome: avatar27_profile,
+    cheerful: avatar27_profile,
+    sad: avatar27_profile,
+  },
+
 ];
